a580162e808f065fb71d37308d7bb3de1d6c4d7a
Lars Giskes <lars.giskes@gmail.com>
2020-02-05 13:30:40 +0100

    added sum method

--- src/main.java
+++ src/main.java
@@ -1,5 +1,12 @@
 public class main {
     public static void main(String args[]){
-        
+        double a = 1.4;
+        double b = 3.9;
+
+        System.out.println(a + " + " + b + " = " + sum(a,b));
+    }
+
+    public static double sum(double a, double b){
+        return a + b;
     }
 }
